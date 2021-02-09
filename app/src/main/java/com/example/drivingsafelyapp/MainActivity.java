9683931f1a08edb6cbaadb48af745f673b9cac8c
Robert Ewing <40344772@live.napier.ac.uk>
2021-02-09 16:37:06 +0000

    accelerometer and gyroscope functionality added

--- app/src/main/java/com/example/drivingsafelyapp/MainActivity.java
+++ app/src/main/java/com/example/drivingsafelyapp/MainActivity.java
@@ -2,28 +2,47 @@ package com.example.drivingsafelyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    private accelerometer accelerometer;
+    private gyroscope gyroscope;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        accelerometer = new accelerometer(this);
+        gyroscope = new gyroscope(this);
+
+        accelerometer.setListener(new accelerometer.Listener() {
+            @Override
+            public void onTranslation(float tx, float ty, float tz) {
+
+            }
+        });
+
+        gyroscope.setListener(new gyroscope.Listener() {
+            @Override
+            public void onRotation(float rx, float ry, float rz) {
+
+            }
+        });
+
         Button buttonB = (Button)findViewById(R.id.startButton);
 
         buttonB.setOnClickListener(new View.OnClickListener(){
 
             public void onClick(View v) {
-                if (buttonB.getText() == "Start") {
-                    buttonB.setText("Stop");
-                }
 
+                buttonB.setText("Stop");
             }
 
         });
